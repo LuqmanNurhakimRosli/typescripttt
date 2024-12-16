171a73ac95ef0d9925d31f7fd72e5ca1783047c2
Luqman Nurhakim <luqmanrosli907@gmail.com>
2024-12-16 11:48:41 +0800

    dk

---
+++ jsx.js
@@ -0,0 +1,7 @@
+//Uppercase letters
+const names = ['luqman', 'nurhakim', 'rosli'];
+
+const uppercaseName = names.map( name =>
+    name.toUpperCase(0) = name.slice(1)
+)
+console.log(uppercaseName);
